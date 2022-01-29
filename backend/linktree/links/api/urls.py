21cc5d136af74fdc37699a3efceacedb45e9a476
berk-12 <36491172+berk-12@users.noreply.github.com>
2022-01-29 23:51:55 +0300

    Add instance enpoint url

--- backend/linktree/links/api/urls.py
+++ backend/linktree/links/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from links.api import views as api_views
 
 urlpatterns = [
-    path('links/', api_views.link_list_create_api_view, name='link-list')
+    path('links/', api_views.link_list_create_api_view, name='link-list'),
+    path('links/<int:pk>', api_views.link_detail_api_view, name='link-detail')
 ]
