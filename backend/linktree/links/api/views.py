42601babfc1163fa4614c102255d93419d003870
berk-12 <36491172+berk-12@users.noreply.github.com>
2022-01-29 23:36:09 +0300

    Add link instance methods

--- backend/linktree/links/api/views.py
+++ backend/linktree/links/api/views.py
@@ -7,9 +7,10 @@ from rest_framework.decorators import api_view
 from links.models import Links
 from links.api.serializers import LinkSerializer
 
+
 @api_view(['GET', 'POST'])
 def link_list_create_api_view(request):
-    
+
     if request.method == 'GET':
         links = Links.objects.all()
         serializer = LinkSerializer(links, many=True)
@@ -21,3 +22,37 @@ def link_list_create_api_view(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def link_detail_api_view(request, pk):
+    try:
+        link_instance = Links.objects.get(pk=pk)
+    except Links.DoesNotExist:
+        return Response({
+            'errors': {
+                'code': 404,
+                'message': 'There is no link with ({pk}) in link tree.'
+            }
+        },
+            status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = LinkSerializer(link_instance)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = LinkSerializer(link_instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status.HTTP_404_NOT_FOUND)
+
+    elif request.method == 'DELETE':
+        link_instance.delete()
+        return Response({
+            'Action': {
+                'code': 204,
+                'message': 'Link has deleted with id ({pk}) from link tree.'
+            }
+        }, status=status.HTTP_204_NO_CONTENT)
